25a361a00b7fb5117de15b4417b7685866972ced
D4fto <163424876+D4fto@users.noreply.github.com>
2024-08-22 11:54:42 -0300

    up

---
+++ 22-08-2024/imv2/imc.js
@@ -0,0 +1,8 @@
+function getImc(){
+    peso = Number(document.getElementById('peso').value)
+    altura = Number(document.getElementById('altura').value)
+    let imc = peso/(altura**2)
+    console.log(imc)
+    document.getElementById('imc').innerText=imc
+}
+document.getElementById("calc").onclick = getImc
