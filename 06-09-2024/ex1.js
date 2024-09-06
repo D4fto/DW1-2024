b2c787bb638c746184033ccb106a7c7c272d1faa
D4fto <163424876+D4fto@users.noreply.github.com>
2024-09-06 15:11:56 -0300

    update

---
+++ 06-09-2024/ex1.js
@@ -0,0 +1,23 @@
+let condicao1 = false
+let condicao2 = false
+let condicao3 = false
+
+console.log(1)
+console.log(2)
+
+if (condicao1) {
+    if (condicao2) {
+        console.log('a')
+    } else {
+        console.log('b')
+    }
+} else {
+    if (condicao3) {
+        console.log('c')
+    } else {
+        console.log('d')
+    }
+}
+
+console.log(3)
+console.log(4)
---
+++ 06-09-2024/ex2.js
@@ -0,0 +1,19 @@
+console.log(1)
+console.log(2)
+
+if (condicao1) {
+    console.log('a')
+} else {
+    if (condicao2) {
+        if (condicao3) {
+            console.log('b')
+        } else {
+            console.log('c')
+        }
+    } else {
+        console.log('d')
+    }
+}
+
+console.log(3)
+console.lo(4)
